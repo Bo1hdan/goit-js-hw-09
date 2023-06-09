55e837e76d6015228da807107b8f89027ee5f9ae
Bo1hdan <87267253+Bo1hdan@users.noreply.github.com>
2023-06-09 20:06:52 +0000

    Deploying to gh-pages from @ Bo1hdan/goit-js-hw-09@a10cb533a564c51f09d0eade2aad5cf6c0b539fc 🚀

---
+++ 01-color-switcher.7812353c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),a=document.querySelector("button[data-stop]");let o,r=!1;e.addEventListener("click",(()=>{r||(r=!0,e.disabled=!0,o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3))})),a.addEventListener("click",(()=>{r=!1,e.disabled=!1,clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.7812353c.js.map
