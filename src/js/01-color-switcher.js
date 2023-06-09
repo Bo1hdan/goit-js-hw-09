a10cb533a564c51f09d0eade2aad5cf6c0b539fc
Bo1hdan <87267253+Bo1hdan@users.noreply.github.com>
2023-06-09 14:04:01 -0600

    first task is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let intervalId;
+let isActive = false;
+
+startBtn.addEventListener('click', () => {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  startBtn.disabled = true;
+
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  isActive = false;
+  startBtn.disabled = false;
+
+  clearInterval(intervalId);
+});
